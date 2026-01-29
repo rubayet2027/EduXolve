80c5487404dd320020154c21da53567bff249cf6
rubayet2027 <u2204002@student.cuet.ac.bd>
2026-01-29 16:50:08 +0600

    routing fixed

--- Eduxolve-CLient/src/components/common/Navbar.jsx
+++ Eduxolve-CLient/src/components/common/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
   const handleLogout = async () => {
     try {
       await logout()
-      navigate('/login')
+      navigate('/')
     } catch (error) {
       console.error('Logout failed:', error)
     }
